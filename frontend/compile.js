f557a82368fef161e89c14f30a2ad20de680db57
Siobhan Kelly <siobhanunakelly1@gmail.com>
2021-03-03 21:33:33 +0000

    m

--- frontend/compile.js
+++ frontend/compile.js
@@ -29,7 +29,3 @@ const tempFile = JSON.parse(solc.compile(JSON.stringify(input)));
 const contractFile = tempFile.contracts['deliveryContract.sol']['Delivery'];
 module.exports = contractFile;
 
-//module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts['deliveryContract.sol'];
-//console.log(module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts['deliveryContract.sol']);
-//console.log(JSON.parse(solc.compile(JSON.stringify(input))));
-//console.log((solc.compile(JSON.stringify(input))));
