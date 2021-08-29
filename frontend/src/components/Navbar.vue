f06bd0cda9f09d54a99272dc8176969d051b64d2
Siobhan Kelly <siobhanunakelly1@gmail.com>
2021-08-29 21:58:54 +0100

    hide navigation drawer from login and register

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav>
         <v-app-bar app>
-            <v-app-bar-nav-icon @click="drawer = !drawer" class="primary--text"></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon v-if="!['login', 'register', 'registerDetails'].includes($route.name)" @click="drawer = !drawer" class="primary--text"></v-app-bar-nav-icon>
             <v-toolbar-title class="text-uppercase primary--text">
                 <span>Transporter</span>
             </v-toolbar-title>
@@ -13,7 +13,7 @@
             </v-btn>
             </div>
         </v-app-bar>
-        <v-navigation-drawer app color = accent v-model="drawer">
+        <v-navigation-drawer v-if="!['login', 'register', 'registerDetails'].includes($route.name)" app color = accent v-model="drawer">
             <v-list>
                 <v-list-item-group v-model="group">
                     <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
